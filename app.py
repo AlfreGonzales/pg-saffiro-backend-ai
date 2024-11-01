89e0a729b32a14e648a540b99caa8237879d32dc
AlfreGonzales <alfregonzales2003@gmail.com>
2024-11-01 16:40:15 -0400

    swagger

--- app.py
+++ app.py
@@ -1,27 +1,60 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+from flasgger import Swagger
 
-# Cargar el modelo entrenado
 with open('model/modelo_tareas.pkl', 'rb') as f:
     model = pickle.load(f)
 
 app = Flask(__name__)
+swagger = Swagger(app)
+
 
 @app.route('/predecir', methods=['POST'])
 def predecir():
-    # Obtener los datos de la petición, que ahora se espera que sea una lista de objetos
+    """
+    Predicción de tareas.
+    ---
+    parameters:
+      - name: tareas
+        in: body
+        required: true
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              tipo:
+                type: number
+                example: 1
+              tiempo:
+                type: number
+                example: 16
+              peso:
+                type: number
+                example: 1500
+              bugs:
+                type: number
+                example: 3
+    responses:
+      200:
+        description: Resultados de las predicciones
+        schema:
+          type: object
+          properties:
+            resultados:
+              type: array
+              items:
+                type: number
+                example: 1.0
+    """
     data = request.get_json()
 
-    # Convertir los datos en un array de predicción adecuado para el modelo
     datos_tareas = np.array([[tarea['tipo'], tarea['tiempo'], tarea['peso'], tarea['bugs']] for tarea in data])
 
-    # Hacer las predicciones con el modelo cargado
     predicciones = model.predict(datos_tareas)
 
-    # Retornar las predicciones como respuesta JSON
     return jsonify({'resultados': predicciones.tolist()})
 
 if __name__ == '__main__':
     app.run(debug=True)
-
